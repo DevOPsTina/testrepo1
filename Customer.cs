2ca57809ef99bc45d851e34b5d93eff092db6d8b
DevOpsTina <ting.alwaysmiler@gmail.com>
2022-09-13 23:19:14 +0530

    this is modified by developer2

--- Customer.cs
+++ Customer.cs
@@ -1,2 +1,8 @@
 dev2
 I am modifing
+this is just test for new merging
+this modified by dev2
+this is not dev1 it is dev2 for test
+just checking and testing
+this is the No. 7 line
+let compare with dev1 and merge1
